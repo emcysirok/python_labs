d68e72875523d84e28078f050ed8707c185a8a44
emcysirok <emtiktak2001@gmail.com>
2025-09-26 02:08:37 +0300

    делит

--- src/lab01/02_sum_avg.py
+++
@@ -1,9 +0,0 @@
-a = input('a: ')
-b = input('b: ')
-a = a.replace(',','.')
-b = b.replace(',','.')
-a = float(a)
-b = float(b)
-su = a+b
-avg = su/2
-print(f'sum={su:.2f}; avg={avg:.2f}')
--- src/lab01/04_minutes_to_hhmm.py
+++
@@ -1,4 +0,0 @@
-minu = int(input('Минуты: '))
-hours = minu//60
-mi = minu%60
-print(f'{hours}:{mi:02d}')
